b3695048f32d9a86ebcc79ad1963ef7f3a15c6dd
jaloul45 <161324338+jaloul45@users.noreply.github.com>
2024-02-26 14:11:41 +0100

    Update meteo.py

--- meteo.py
+++ meteo.py
@@ -6,9 +6,17 @@ while True:
   devices = scanner.scan(timeout=3.0)
 
   for device in devices:
-    print(
-      f"Device found {device.addr} ({device.addrType}), "
-      f"RSSI={device.rssi} dB"
-    )
-    for adtype, description, value in device.getScanData():
-      print(f" ({adtype}) {description} = {value}")
+    if device.addr == "d6:1c:bf:b7:76:62" or device.addr == "d7:ef:13:27:15:29" or device.addr == "d6:c6:c7:39:a2:e8":
+      print(
+        f"Device found {device.addr} ({device.addrType}), "
+        f"RSSI={device.rssi} dB"
+      )
+      for adtype, description, value in device.getScanData():
+        if adtype == 22 :
+          Battery = int(value[20:22], 16)
+          Temperature = int(value[24:28], 16) / 100
+          Humidity = int(value[28:32], 16)
+
+          print("Battery : ", Battery, " %")
+          print("Temperature : ", Temperature, " °C")
+          print("Humidity : ", Humidity, " %")
