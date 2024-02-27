a9d50f65d58facc47dd11cd01240aa473002f723
jaloul45 <161324338+jaloul45@users.noreply.github.com>
2024-02-27 13:37:59 +0100

    Update meteo.py

--- meteo.py
+++ meteo.py
@@ -1,15 +1,17 @@
 from bluepy.btle import Scanner
 from peewee import MySQLDatabase, Model, CharField, IntegerField, FloatField
 
-db = MySQLDatabase('msql', user='pi', password='raspberry', host='localhost')
+db = MySQLDatabase("msql", user="pi", password="raspberry", host="localhost")
+
 
 class Data(Model):
     temperature = FloatField()
-    humidite = FloatField()
+    humidity = FloatField()
     battery = FloatField()
 
     class Meta:
         database = db
+
 
 db.connect()
 db.create_tables([Data])
@@ -18,20 +20,26 @@ db.close()
 scanner = Scanner()
 print("Begin device scan")
 while True:
-  devices = scanner.scan(timeout=3.0)
+    devices = scanner.scan(timeout=3.0)
 
-  for device in devices:
-    if device.addr == "d6:1c:bf:b7:76:62" or device.addr == "d7:ef:13:27:15:29" or device.addr == "d6:c6:c7:39:a2:e8":
-      print(
-        f"Device found {device.addr} ({device.addrType}), "
-        f"RSSI={device.rssi} dB"
-      )
-      for adtype, description, value in device.getScanData():
-        if adtype == 22 :
-          Battery = int(value[20:22], 16)
-          Temperature = int(value[24:28], 16) / 100
-          Humidity = int(value[28:32], 16) / 100
+    for device in devices:
+        if (
+            device.addr == "d6:1c:bf:b7:76:62"
+            or device.addr == "d7:ef:13:27:15:29"
+            or device.addr == "d6:c6:c7:39:a2:e8"
+        ):
+            print(
+                f"Device found {device.addr} ({device.addrType}), "
+                f"RSSI={device.rssi} dB"
+            )
+            for adtype, description, value in device.getScanData():
+                if adtype == 22:
+                    Battery = int(value[20:22], 16)
+                    Temperature = int(value[24:28], 16) / 100
+                    Humidity = int(value[28:32], 16) / 100
 
-          print("Battery : ", Battery, " %")
-          print("Temperature : ", Temperature, " °C")
-          print("Humidity : ", Humidity, " %")
+                    print("Battery : ", Battery, " %")
+                    print("Temperature : ", Temperature, " °C")
+                    print("Humidity : ", Humidity, " %")
+
+                    Data.create(temperature=Temperature, battery=Battery, humidity=Humidity)
